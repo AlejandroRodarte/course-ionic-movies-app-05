5a9e7b1be092fea075e7a60067a1f347b28460c2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-22 22:07:40 -0600

    138. Mostrar detalles de la película

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
+import { MovieDetails, MovieActors } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-detail',
@@ -11,13 +12,17 @@ export class DetailComponent implements OnInit {
   @Input()
   public id: number;
 
+  public movieDetails: MovieDetails;
+
+  public movieActors: MovieActors;
+
   constructor(
     private movieService: MoviesService
   ) { }
 
   ngOnInit() {
-    this.movieService.getMovieDetails(this.id).subscribe(console.log);
-    this.movieService.getMovieActors(this.id).subscribe(console.log);
+    this.movieService.getMovieDetails(this.id).subscribe((movieDetails: MovieDetails) => this.movieDetails = movieDetails);
+    this.movieService.getMovieActors(this.id).subscribe((movieActors: MovieActors) => this.movieActors = movieActors);
   }
 
 }
